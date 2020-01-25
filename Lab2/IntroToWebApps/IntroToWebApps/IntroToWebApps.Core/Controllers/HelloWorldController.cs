eb5d666d56f75ea587a074d38e41c842ebc32827
Patrick <pbobyn@gmail.com>
2020-01-25 12:02:00 -0500

    Adding lab2 to github.

---
+++ Lab2/IntroToWebApps/IntroToWebApps/IntroToWebApps.Core/Controllers/HelloWorldController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using IntroToWebApps.Core.Models;
+
+namespace IntroToWebApps.Core.Controllers.Core.Controllers
+{
+    [Route("[controller]"), Route("/")]
+    public class HelloWorldController : Controller
+    {
+        [HttpGet]
+        [Route("[action]"), Route("")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="person"></param>
+        /// <returns></returns>
+        [HttpPost]
+        public IActionResult Index(Person person)
+        {
+            return View(person);
+        }
+    }
+}
---
+++ Lab2/IntroToWebApps/IntroToWebApps/IntroToWebApps.Core/Models/Person.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IntroToWebApps.Core.Models
+{
+    public class Person
+    {
+        [Required]
+        [StringLength(100)]
+        public string FirstName
+        {
+            get;
+            set;
+        }
+
+        [Required]
+        [StringLength(100)]
+        public string LastName
+        {
+            get;
+            set;
+        }
+    }
+}
